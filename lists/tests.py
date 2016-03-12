ce77fee11b40d3b8233b1efec03a3384eaf08a6c
sharpscar <sharpscar@gmail.com>
2016-03-12 09:20:57 +0900

    최소 html을 반환하는 기본 뷰

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,10 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists.views import home_page
+
+
+
 
 class HomePageTest(TestCase):
 
@@ -8,3 +12,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    # 올바른 html형식의 실제 응답을 반환하는 함수를 위한 테스트
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
