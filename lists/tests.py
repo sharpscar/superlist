8239e843e23a327d2f3e62ced1047e139eb54d37
sharpscar <sharpscar@gmail.com>
2016-03-12 09:12:00 +0900

    첫 단위 테스트와 url 맵핑 그리고 임시 뷰

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,10 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+
+    # URL의 사이트 루트를 해석해서 특정 뷰 기능에 매칭시킬수 있는가
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import  patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns('',
+        # Examples:
+         url(r'^$', 'lists.views.home_page', name='home'),
+        # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-]
+        #url(r'^admin/', include(admin.site.urls)),
+)
