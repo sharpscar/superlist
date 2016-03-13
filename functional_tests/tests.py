ca4f9f9934139696e54c75b2a9112df6921de945
sharpscar <sharpscar@gmail.com>
2016-03-13 20:27:15 +0900

    레이아웃과 스타일링을 위한 기능테스트 첫단계

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -88,5 +88,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertIn('우유 사기', page_text)
 
+    def test_layout_and_style(self):
+        #에디스는 메인 페이지를 방문한다.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #그녀는 입력상자가 가운데 배치된것을 본다.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x']+ inputbox.size['width']/2, 512, delta=10)
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,512, delta=10)
 
         #둘다 만족하고 잠자리에 든다
