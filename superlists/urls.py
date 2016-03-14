6287fc7f28d4cf9ec9398ade3c756f239e7fbe98
sharpscar <sharpscar@gmail.com>
2016-03-15 06:08:26 +0900

    템플릿에 있는 하드 코딩된 URL 부분 리펙터링

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
          url(r'^$', 'lists.views.home_page', name='home'),
-         #url(r'^lists/', include('lists.urls')),
+         url(r'^lists/', include('lists.urls')),
 
-         url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-         
-         url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+        #  url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+         #
+        #  url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
 )
