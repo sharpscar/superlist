4d486680a567071040cb0b23eeda7f555f6eae88
sharpscar <sharpscar@gmail.com>
2016-03-15 04:13:53 +0900

    기능테스트를 개별 파일, 개별 클래스로 분할

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,18 @@
+from .base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+
+
+    def test_layout_and_style(self):
+        #에디스는 메인 페이지를 방문한다.
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #그녀는 입력상자가 가운데 배치된것을 본다.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x']+ inputbox.size['width']/2, 512, delta=10)
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,512, delta=10)
