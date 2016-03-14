05bd19cab6fbce58f13eb68a708b01680a419444
sharpscar <sharpscar@gmail.com>
2016-03-15 04:27:24 +0900

    단위테스트를 두개 파일로 나누기 models.py, views.py

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -14,38 +14,6 @@ from lists.views import home_page
 # sys.setdefaultencoding('utf8')
 
 
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-
-        first_item = Item()
-        first_item.text = '첫번째 아이템'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = '두번째 아이템'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '첫번째 아이템')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, '두번째 아이템')
-        self.assertEqual(second_saved_item.list, list_)
-
-
 
 class HomePageTest(TestCase):
 
@@ -77,7 +45,7 @@ class NewListTest(TestCase):
             new_list  = List.objects.first()
             self.assertRedirects(response,'/lists/%d/' % (new_list.id,))
 
-    
+
 
 class ListViewTest(TestCase):
 
