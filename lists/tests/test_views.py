17262778dff463f25dd3cd495e6cfb1cd1ffe776
sharpscar <sharpscar@gmail.com>
2016-03-15 12:57:49 +0900

    home_page에서 신규 폼 사용 및 테스트 단순화. 하지만 정상동작하지 않음

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -9,6 +9,7 @@ from django.http import HttpRequest
 from lists.models import Item, List
 from lists.views import home_page
 from django.utils.html import escape
+from lists.forms import ItemForm
 # import sys
 # reload(sys)
 # sys.setdefaultencoding('utf8')
@@ -17,17 +18,14 @@ from django.utils.html import escape
 
 class HomePageTest(TestCase):
 
-    # URL의 사이트 루트를 해석해서 특정 뷰 기능에 매칭시킬수 있는가
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    # 올바른 html형식의 실제 응답을 반환하는 함수를 위한 테스트
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html  =  render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 class NewListTest(TestCase):
         # home_page함수가 post요청을 처리, 저장할수 있도록 테스트
